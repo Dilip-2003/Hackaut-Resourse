d1f37787c48a4a1581952bec061fbe30f5ba03d2
Dilip-2003 <darkerdip@gmail.com>
2023-08-17 02:31:45 +0530

    second changes

--- script.js
+++ script.js
@@ -2,37 +2,38 @@
 
     const resources = [
         // { title:"Topic","" }
-        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/html/default.asp",link2:"https://developer.mozilla.org/en-US/docs/Web/HTML",link3:"https://youtu.be/BsDoLVMnmZs",link:"./resources/HTML5NotesForProfessionals.pdf"},
 
-        {title1:"CSS",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"CSS",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/css/default.asp",link2:"https://developer.mozilla.org/en-US/docs/Web/CSS",link3:"https://youtu.be/Edsxf_NBFrw",link:"./resources/CSSNotesForProfessionals.pdf"},
 
-        {title1:"JavaScript",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"JavaScript",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/js/default.asp",link2:"https://developer.mozilla.org/en-US/docs/Web/JavaScript",link3:"https://www.youtube.com/watch?v=Lgxgm-T9cgA&list=PL0b6OzIxLPbx-BZTaWu_AF7hsKo_Fvsnf",link:"./resources/JavaScriptNotesForProfessionals (1).pdf"},
 
-        {title1:"NodeJs",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"NodeJs ",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/nodejs/default.asp",link2:"https://nodejs.org/en/docs",link3:"https://www.youtube.com/watch?v=LAUi8pPlcUM&list=PLC3y8-rFHvwh8shCMHFA5kWxD9PaPwxaY",link:"./resources/NodeJSNotesForProfessionals.pdf"},
 
-        {title1:"ExpressJs",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
 
-        {title1:"ReactJs",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"ExpressJs",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Introduction",link2:"https://expressjs.com/",link3:"https://www.youtube.com/watch?v=7H_QH9nipNs&pp=ygUTZXhwcmVzcyBqcyB0dXRvcmlhbA%3D%3D",link:"./resources/NodeJSNotesForProfessionals.pdf"},
 
-        {title1:"AngularJs",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"ReactJs",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/react/default.asp",link2:"https://react.dev/learn",link3:"https://www.youtube.com/watch?v=QFaFIcGhPoM&list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3",link:"./resources/ReactJSNotesForProfessionals.pdf"},
 
-        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"AngularJs(No Pdf)",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/angular/default.asp",link2:"https://angular.io/docs",link3:"https://www.youtube.com/watch?v=zKkUN-mJtPQ&list=PL6n9fhu94yhWKHkcL7RJmmXyxkuFB3KSl"},
 
-        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"VueJs (No pdf)",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/vue/index.php",link2:"https://vuejs.org/guide/introduction.html",link3:"https://www.youtube.com/watch?v=ccsz9FRy-nk&list=PLC3y8-rFHvwgeQIfSDtEGVvvSEPDkL_1f"},
 
-        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"jQuery",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/jquery/default.asp",link2:"https://devdocs.io/jquery/",link3:"https://www.youtube.com/watch?v=QvaFeU0LWnc&list=PL0b6OzIxLPbzSyiC0PFaqeabe1aGhfrbW",link:"./resources/jQueryNotesForProfessionals.pdf"},
 
-        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"TypeScript",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/typescript/index.php",link2:"https://www.typescriptlang.org/docs/",link3:"https://www.youtube.com/watch?v=Xciunyug99U&list=PLwGdqUZWnOp0xfHQFmlL52b_6-QZ0mnk_",link:"./resources/TypeScriptNotesForProfessionals.pdf"},
 
-        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"MySQL",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/mysql/default.asp",link2:"https://dev.mysql.com/doc/",link3:"https://www.youtube.com/watch?v=iP1wOSsKjW8&list=PLS1QulWo1RIahlYDqHWZb81qsKgEvPiHn",link:"./resources/MySQLNotesForProfessionals.pdf"},
 
-        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"},
+        {title1:"BootStrap (No Pdf)",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/bootstrap5/index.php",link2:"https://getbootstrap.com/docs/4.1/getting-started/introduction/",link3:"https://www.youtube.com/watch?v=wkSA9bfCmKU&list=PL0b6OzIxLPbz1cgxiH5KCBsyQij1HsPtG"},
 
-        {title1:"HTML",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/",link2:"https://www.w3schools.com/js/js_array_iteration.asp",link:"./ViewGeneratedDocOnFly.pdf"}
-       
-       
-        
-        
+        {title1:"Tailwind CSS(No Pdf)",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://tailwindui.com/documentation",link2:"https://tailwindui.com/documentation",link3:"https://www.youtube.com/watch?v=L4_jarMnB0c&list=PLu0W_9lII9ahwFDuExCpPFHAK829Wto2O"},
+
+        {title1:"Django(No Pdf)",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/django/index.php",link2:"https://docs.djangoproject.com/en/4.2/",link3:"https://www.youtube.com/watch?v=C1NgOmoOszc&list=PLjVLYmrlmjGcyt3m6rt21nfjhYSWP_Ue_"},
+        {title1:"PHP(No Pdf)",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/php/default.asp",link2:"https://www.php.net/docs.php",link3:"https://www.youtube.com/watch?v=at19OmH2Bg4&list=PLu0W_9lII9aikXkRE0WxDt1vozo3hnmtR"},
+        {title1:"Git",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/git/default.asp",link2:"https://git-scm.com/doc",link3:"https://www.youtube.com/watch?v=gwWKnnCMQ5c&pp=ygUOZ2l0IGFuZCBnaXRodWI%3D",link:"./resources/GitNotesForProfessionals.pdf"},
+        {title1:"AWS Cloud(No Pdf)",title2:"Online Resource1",title3:"Online Resource2", title4:"Video Reference", link1:"https://www.w3schools.com/aws/index.php",link2:"https://docs.aws.amazon.com/",link3:"https://www.youtube.com/watch?v=rKNSc8RrwxA&list=PL6XT0grm_TfgtwtwUit305qS-HhDvb4du"}  
+
     ];
 
     const resourceList = $("#resource-list");
@@ -42,8 +43,8 @@
             $("<a>").text(resource.title1),
             $("<a>").attr("href",resource.link1).text(resource.title2),
             $("<a>").attr("href", resource.link2).text(resource.title3),
-            $("<a>").attr("href", resource.link2).text(resource.title4),
-            $("<button>").addClass("download-btn").text("Download Pdf")
+            $("<a>").attr("href", resource.link3).text(resource.title4),
+            $("<button>").addClass("download-btn").text("Download PDF")
            
         );
 
@@ -64,12 +65,14 @@
         link.click();
         document.body.removeChild(link);
     }
+    $("#resource-list a").css({
+        "padding":"5px"
+    });
 
 
 $("#web").click(function(){
 
      $("#web-development").slideToggle(200);
-     $(".web-development").css({
-        "display":"flex"
-     });
+     $(".web-development").css(
+        "display","block");
 });
